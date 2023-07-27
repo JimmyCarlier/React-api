8116b8664fae36a527a2cffb0c1798bb9b1a325a
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-07-27 18:00:26 +0200

    Add show/unShow function for coktails

--- 02_cocktail_react/src/SingleCoktail.jsx
+++ 02_cocktail_react/src/SingleCoktail.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 const SingleCoktail = ({ drink }) => {
-  let table = [];
-  for (let n in drink) {
-    if (n.includes("strIngredient") && drink[n] != null) {
-      table.push(drink[n]);
+  const [show, setShow] = useState([]);
+  const [itShow, setItShow] = useState(false);
+
+  const showIngredient = () => {
+    let table = [];
+    for (let n in drink) {
+      if (n.includes("strIngredient") && drink[n] != null) {
+        table.push(drink[n]);
+      }
     }
-  }
+    setShow(table);
+    setItShow(!itShow);
+  };
+
   return (
     <>
       <h3>{drink.strDrink}</h3>
       <img src={drink.strDrinkThumb} alt="" className="cocktailImg" />
-      {table.map((ingredient) => {
-        return <p>{ingredient}</p>;
-      })}
+      <button onClick={showIngredient}>Show Ingredient</button>
+      {show.length !== 0 &&
+        itShow &&
+        show.map((ingredient) => {
+          return <p>{ingredient}</p>;
+        })}
     </>
   );
 };
