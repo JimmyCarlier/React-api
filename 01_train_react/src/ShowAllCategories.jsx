c8209a4dc0de9af2c9d3e63e060cb9e2380a25f0
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-07-27 10:39:40 +0200

    Add loading useState

--- 01_train_react/src/ShowAllCategories.jsx
+++ 01_train_react/src/ShowAllCategories.jsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from "react";
 
 const ShowAllCategory = () => {
   const [mealCategories, setMealCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchApi = async () => {
-    const resultApi = await fetch(
-      "https://www.themealdb.com/api/json/v1/1/categories.php"
-    );
-    const resultJson = await resultApi.json();
-
-    setMealCategories(resultJson.categories);
+    try {
+      const resultApi = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      const resultJson = await resultApi.json();
+      setMealCategories(resultJson.categories);
+      setLoading(false);
+    } catch {
+      setLoading(true);
+    }
   };
 
   // useEffect permits to reload the fetch api one time, and, don't create infinit loop
@@ -20,14 +25,16 @@ const ShowAllCategory = () => {
   return (
     <>
       <h1>All meals categories</h1>
-      {mealCategories.map((meal) => {
-        return (
-          <div key={meal.idCategory}>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        mealCategories.map((meal) => (
+          <>
             <h2>{meal.strCategory}</h2>
             <img src={meal.strCategoryThumb} alt="" />
-          </div>
-        );
-      })}
+          </>
+        ))
+      )}
     </>
   );
 };
