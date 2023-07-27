ae1a9b1d602d0f523a28bd6fac1d198b954c1530
Jimmy Carlier <jimmy.carlier@lapiscine.pro>
2023-07-27 17:09:02 +0200

    Update

---
+++ 02_cocktail_react/src/AllCoktails.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+const AllCoktails = () => {
+  const [showAllCoktail, setShowAllCoktail] = useState();
+  const [loading, setLoading] = useState(true);
+  const [random, setRandom] = useState([]);
+
+  const fetchCoktailApi = async () => {
+    try {
+      const resultAllCoktails = await fetch(
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+      );
+      const resultCoktailsJson = await resultAllCoktails.json();
+      setShowAllCoktail(resultCoktailsJson.drinks);
+
+      setLoading(false);
+
+      //   let mydrinks = resultCoktailsJson["drinks"];
+      //   let table = [];
+      //   for (let i = 0; i <= mydrinks.length; i++) {
+      //     table[i] = [];
+      //     for (const key in mydrinks[i]) {
+      //       if (mydrinks[i][key] !== null && key.includes("strIngredient")) {
+      //         table[i].push(mydrinks[i][key]);
+      //       }
+      //     }
+      //   }
+    } catch {}
+  };
+
+  const RandomCocktail = async () => {
+    const randomFetch = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
+    const randomJson = await randomFetch.json();
+    setRandom(randomJson.drinks[0]);
+  };
+
+  useEffect(() => {
+    fetchCoktailApi();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        showAllCoktail.map((drink) => (
+          <>
+            <h3>{drink.strDrink}</h3>
+            <img src={drink.strDrinkThumb} alt="" className="cocktailImg" />
+          </>
+        ))
+      )}
+      <div className="randomCock">
+        <button onClick={RandomCocktail}>Random Coktail</button>
+        <h3>{random.strDrink}</h3>
+        <img src={random.strDrinkThumb} alt="" className="cocktailImg" />
+      </div>
+    </>
+  );
+};
+
+export default AllCoktails;
---
+++ 02_cocktail_react/src/App.js
@@ -0,0 +1,12 @@
+import AllCoktails from "./AllCoktails";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <AllCoktails />
+    </>
+  );
+}
+
+export default App;
